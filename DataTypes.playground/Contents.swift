ea3eb08d3a7d21549b8e75513d1b2b229d41e8a4
Ammyy9908 <sb78639@gmail.com>
2021-10-19 15:03:41 +0530

    intial commit

---
+++ DataTypes.playground/Contents.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+/*
+ 
+ String
+ Int
+ Float
+ Double
+ Bool
+ 
+ */
+
+
+var firstName:String = "Sumit" // string
+
+print(firstName)
+
+var score:Int = 100
+
+var cgpa:Float = 6.7
+
+var isRainy:Bool = true
+
+var salary:Double = 6.7778
+
+
+
+
---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+var firstname = "Sumit"
+
+print(firstname)
+
+var stock_price = 100
+
+print(stock_price)
+
+stock_price = 50
+
+print(stock_price)
+
+let lastname = "Bighaniya"
+
+print(lastname)
+
+// Data Types
+
+
+
+
+
